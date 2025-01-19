f39c752ba62aeccc42452dbfc1f7df7d0fbd3d14
PR51 <pr51@ford.com>
2025-01-19 22:43:20 +0530

    new update
    new update

--- src/app/api/file/route.ts
+++ src/app/api/file/route.ts
@@ -1,18 +1,19 @@
 import { list } from "@vercel/blob";
-import { revalidateTag } from 'next/cache';
 
 const MY_BLOB_TOKEN = process.env.BLOB_READ_WRITE_TOKEN; // **REPLACE "YOUR_ACTUAL_TOKEN"**
 
 export async function GET() {
     try {
         const { blobs } = await list({ token: MY_BLOB_TOKEN });
+        const cacheControl = 'no-cache, no-store, must-revalidate';
+        const headers = new Headers({
+            'Content-Type': 'application/json',
+            'Cache-Control': cacheControl,
+            'Pragma': 'no-cache',
+            'Expires': '0'
+        });
         return new Response(JSON.stringify(blobs), {
-            headers: {
-                'Content-Type': 'application/json',
-                'Cache-Control': 'no-cache, no-store, must-revalidate',
-                'Pragma': 'no-cache',
-                'Expires': '0'
-            },
+            headers: headers,
         });
     } catch (error) {
         console.error("Error listing blobs:", error);
@@ -20,8 +21,3 @@ export async function GET() {
         return new Response(`Error: ${errorMessage}`, { status: 500 });
     }
 }
-
-
-export async function action() {
-    revalidateTag('blobs');
-  }
